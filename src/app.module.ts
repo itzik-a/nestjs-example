6ee31fd882da4b6a59011f8916ece56e302d9869
Itzik <snufkin.a89@gmail.com>
2021-07-04 08:23:30 +0300

    add config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { AppController } from './app.controller'
 import { AppDummy } from './app.dummy'
 import { AppJapanService } from './app.japan.service'
 import { AppService } from './app.service'
-import { Event } from './events/event.entity'
+import ormConfig from './config/orm.config'
+import ormConfigProd from './config/orm.config.prod'
 import { EventsModule } from './events/events.module'
 
 const isJapanese = true
@@ -12,15 +14,15 @@ const APP_SERVICE_CLASS = isJapanese ? AppJapanService : AppService
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: '127.0.0.1',
-      port: 5432,
-      username: 'postgres',
-      password: 'example',
-      database: 'nest-event',
-      entities: [Event],
-      synchronize: true, // careful - this syncs db schema with entities
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env', // this is the default
+      load: [ormConfig],
+      expandVariables: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory:
+        process.env.NODE_ENV !== 'production' ? ormConfig : ormConfigProd,
     }),
     EventsModule,
   ],

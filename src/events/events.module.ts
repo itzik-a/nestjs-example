65f9006b681c325bc9d31d561741c4f12858b57c
Itzik <snufkin.a89@gmail.com>
2021-07-10 13:41:30 +0300

    practice relations

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
+import { Attendee } from './attendee.entity'
 import { Event } from './event.entity'
 import { EventsController } from './events.controller'
 import { EventsService } from './events.service'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Event])],
+  imports: [TypeOrmModule.forFeature([Event, Attendee])],
   controllers: [EventsController],
   providers: [EventsService],
 })
--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -4,19 +4,22 @@ import { UpdateEventDto } from './update-event.dto'
 import { Event } from './event.entity'
 import { Like, MoreThan, Repository } from 'typeorm'
 import { InjectRepository } from '@nestjs/typeorm'
+import { Attendee } from './attendee.entity'
 
 @Injectable()
 export class EventsService {
   constructor(
-    @InjectRepository(Event) private readonly repository: Repository<Event>,
+    @InjectRepository(Event) private readonly eventRepo: Repository<Event>,
+    @InjectRepository(Attendee)
+    private readonly attendeeRepo: Repository<Attendee>,
   ) {}
 
   async findAll() {
-    return await this.repository.find()
+    return await this.eventRepo.find()
   }
 
   async practice() {
-    return await this.repository.find({
+    return await this.eventRepo.find({
       select: ['id', 'description'],
       where: [{ id: MoreThan(2) }, { description: Like('%#1%') }],
       take: 2,
@@ -28,11 +31,19 @@ export class EventsService {
   }
 
   async practice2() {
-    return await this.repository.findOne(1, { relations: ['attendees'] })
+    // return await this.eventRepo.findOne(1, { relations: ['attendees'] })
+    const event = await this.eventRepo.findOne(1, { relations: ['attendees'] })
+    const attendee = new Attendee()
+    attendee.name = 'Jerry using cascade'
+    event.attendees.push(attendee)
+
+    await this.eventRepo.save(event)
+
+    return event
   }
 
   async findOne(id: number) {
-    const event = await this.repository.findOne(id)
+    const event = await this.eventRepo.findOne(id)
     if (!event) {
       throw new NotFoundException()
     }
@@ -40,15 +51,15 @@ export class EventsService {
   }
 
   async create(input: CreateEventDto) {
-    return await this.repository.save({
+    return await this.eventRepo.save({
       ...input,
       date: new Date(input.date),
     })
   }
 
   async update(id: string, input: UpdateEventDto) {
-    const event = await this.repository.findOne(id)
-    return await this.repository.save({
+    const event = await this.eventRepo.findOne(id)
+    return await this.eventRepo.save({
       ...event,
       ...input,
       date: input.date ? new Date(input.date) : event.date,
@@ -56,10 +67,10 @@ export class EventsService {
   }
 
   async remove(id: string) {
-    const event = await this.repository.findOne(id)
+    const event = await this.eventRepo.findOne(id)
     if (!event) {
       throw new NotFoundException()
     }
-    await this.repository.remove(event)
+    await this.eventRepo.remove(event)
   }
 }
